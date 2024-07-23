c10a87d37d1d59f97d4d5f93232ed5fd38ff9687
JWJung-99 <39busy@naver.com>
2024-07-23 20:39:03 +0900

    🐛 버그 수정

--- workspace/community/src/app/loading.tsx
+++ workspace/community/src/app/loading.tsx
@@ -1,5 +1,12 @@
-import Spinner from "@/components/Spinner";
-
-export default function Loading() {
-  return <Spinner />;
+export default function Spinner() {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-white bg-opacity-10">
+      <div className="flex flex-col items-center">
+        <h3 className="mb-4 text-lg font-semibold">
+          로딩 중... 잠시만 기다려주세요.
+        </h3>
+        {/* <HashLoader color="#F97316" /> */}
+      </div>
+    </div>
+  );
 }
